6ee3c303c4ac6d975d818df0581b0d22c06500a7
ikeyu0806 <ikeyu0806@gmail.com>
2024-12-22 13:01:17 +0900

    add use_default_mobility_album/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import React from 'react'
 export default function Home() {
   return (
     <>
+      <a href="/use_default_mobility_album">use client/server指定なしでアルバムを表示</a>
+      <br />
       <a href="/use_server_mobility_album">サーバサイドレンダリングでアルバムを表示</a>
       <br />
       <a href="/use_client_mobility_album">クライアントサイドレンダリングでアルバムを表示</a>
---
+++ app/use_default_mobility_album/page.tsx
@@ -0,0 +1,13 @@
+import MobilityshowAlbum from '../../components/mobility_show_album'
+
+export default function UseDefaultMobilityAlbum() {
+  return (
+    <>
+      <MobilityshowAlbum />
+      <MobilityshowAlbum />
+      <MobilityshowAlbum />
+      <MobilityshowAlbum />
+      <MobilityshowAlbum />
+    </>
+  )
+}
