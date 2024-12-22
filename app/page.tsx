3b489bd377ddb63ad403ba2e67e471ead3c9aca2
ikeyu0806 <ikeyu0806@gmail.com>
2024-12-22 17:13:50 +0900

    update README

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,15 @@ export default function Home() {
     <>
       <a href="/use_default_mobility_album">use client/server指定なしでアルバムを表示</a>
       <br />
-      <a href="/use_server_mobility_album">サーバサイドレンダリングでアルバムを表示</a>
+      <a href="/use_server_mobility_album">サーバサイドコンポーネントでアルバムを表示</a>
       <br />
-      <a href="/use_client_mobility_album">クライアントサイドレンダリングでアルバムを表示</a>
+      <a href="/use_client_mobility_album">クライアントコンポーネントでアルバムを表示</a>
       <br />
       <a href="/use_default_heavy_computation">use client/server指定なしで重い計算を実行</a>
       <br />
-      <a href="/use_server_heavy_computation">サーバサイドレンダリングで重い計算を実行</a>
+      <a href="/use_server_heavy_computation">サーバサイドコンポーネントで重い計算を実行</a>
       <br />
-      <a href="/use_client_heavy_computation">クライアントサイドレンダリングで重い計算を実行</a>
+      <a href="/use_client_heavy_computation">クライアントコンポーネントで重い計算を実行</a>
     </>
   )
 }
