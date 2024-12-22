4f9a105f9bcf17b38e264fc1d22d36bc339fc1b2
ikeyu0806 <ikeyu0806@gmail.com>
2024-12-22 11:28:03 +0900

    create mobility_show_album component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import MobilityshowAlubum from '../components/mobility_show_album'
 
 export default function Home() {
   return (
-    <>Next album App</>
+    <>
+      <MobilityshowAlubum />
+    </>
   )
 }
