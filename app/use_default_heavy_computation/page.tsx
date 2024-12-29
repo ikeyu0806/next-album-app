9ff95c6e2edcec623b7e76b001d139ba970bd370
ikeyu0806 <ikeyu0806@gmail.com>
2024-12-29 10:42:45 +0900

    remove use default pages

--- app/use_default_heavy_computation/page.tsx
+++
@@ -1,9 +0,0 @@
-import HeavyComputation from '../../components/heavy_computation'
-
-export default function UseClientMobilityAlbum() {
-  return (
-    <>
-      <HeavyComputation />
-    </>
-  )
-}
