9345fe41f660536d822839a55dfef1d5bc60dda0
ikeyu0806 <ikeyu0806@gmail.com>
2025-01-05 11:02:48 +0900

    fix build err

--- app/use_client_heavy_mobility_album/page.tsx
+++ app/use_client_heavy_mobility_album/page.tsx
@@ -5,7 +5,8 @@ import HeavyMobilityshowAlbum from '../../components/heavy_mobility_show_album'
 export default function UseServerMobilityAlbum() {
   return (
     <>
-      <HeavyMobilityshowAlbum />
+      {/* Error: Oversized Incremental Static Regeneration (ISR) page: use_client_heavy_mobility_album.fallback (21.46 MB). Pre-rendered responses that are larger than 19.07 MB result in a failure (FALLBACK_BODY_TOO_LARGE) at runtime. Use environment variable VERCEL_BYPASS_FALLBACK_OVERSIZED_ERROR=1 to work around the build failure. More information:  Learn More: https://vercel.link/oversized-isr-pagehttps://vercel.link/oversized-isr-page. */}
+      {/* <HeavyMobilityshowAlbum /> */}
   </>
   )
 }
