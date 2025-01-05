5a8b310fce53a935faa74dd96e3db4f62c85d3eb
ikeyu0806 <ikeyu0806@gmail.com>
2025-01-05 11:11:16 +0900

    reduce heavy album image

--- app/use_client_heavy_mobility_album/page.tsx
+++ app/use_client_heavy_mobility_album/page.tsx
@@ -6,7 +6,7 @@ export default function UseServerMobilityAlbum() {
   return (
     <>
       {/* Error: Oversized Incremental Static Regeneration (ISR) page: use_client_heavy_mobility_album.fallback (21.46 MB). Pre-rendered responses that are larger than 19.07 MB result in a failure (FALLBACK_BODY_TOO_LARGE) at runtime. Use environment variable VERCEL_BYPASS_FALLBACK_OVERSIZED_ERROR=1 to work around the build failure. More information:  Learn More: https://vercel.link/oversized-isr-pagehttps://vercel.link/oversized-isr-page. */}
-      {/* <HeavyMobilityshowAlbum /> */}
+      <HeavyMobilityshowAlbum />
   </>
   )
 }
