e9fe457e518f2035bade706cf2c2216cbd1d9c40
ikeyu0806 <ikeyu0806@gmail.com>
2025-02-02 09:10:04 +0900

    add slow_response api

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,8 @@ export default function Home() {
       <a href="/use_server_heavy_computation">サーバサイドコンポーネントで重い計算を実行</a>
       <br />
       <a href="/use_client_light_computation">クライアントコンポーネントで軽い計算を実行</a>
+      <br />
+      <a href="/api/slow_response">パフォーマンス計測用APIエンドポイント</a>
     </>
   )
 }
